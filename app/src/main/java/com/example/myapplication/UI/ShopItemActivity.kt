9174c2e81ff7da03b4f2ae74adfd0e0e8138cf54
Crea702 <sir.02.aib@gmail.com>
2024-06-27 12:59:59 +0500

    реализация второго экрана(xml)

---
+++ app/src/main/java/com/example/myapplication/UI/ShopItemActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.UI
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+
+    }
+}
